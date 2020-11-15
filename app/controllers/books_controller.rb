0e126d01b2de5f3ff90b4d1252affd0bde4294db
Masahiro913 <gonzaes0819@yahoo.co.jp>
2020-11-15 14:31:57 +0900

    ヘッダーの作成、メーラーの準備、ブックスコントローラーの準備

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+  def index
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book.create(books_params) 
+  end
+
+  private
+  def books_params
+    permit().merge(user_id: current_user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates_uniqueness_of :email
+    validates :password, format: { with: /\A[a-z0-9]+\z/i} 
+    validates :nickname
+  end
 end
